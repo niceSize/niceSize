cb94428a720807a67e197b2dc257f0e7984c7b1a
Locxter <54595101+Locxter@users.noreply.github.com>
2019-09-08 14:02:42 +0200

    testing

--- docs/js/niceSize.js
+++ docs/js/niceSize.js
@@ -28,19 +28,19 @@ function initializeNiceSize() {
 	if(parseFloat(window.getComputedStyle(document.getElementById('niceSize')).getPropertyValue('height')) > window.innerHeight){
 		document.querySelector('body').style.setProperty('--nSHeight', window.innerHeight * 0.01 + 'px');
 		widthOld = window.innerWidth;
-		alert('if');
-		alert(widthOld);
+		//alert('if');
+		//alert(widthOld);
 		mobile=1;
 	}else{
 		document.querySelector('body').style.setProperty('--nSHeight', window.innerHeight * 0.01 + 'px');
 		mobile=0;
-		alert('else');
+		//alert('else');
 	}
 	document.getElementById('niceSize').style.setProperty('display', 'none');
 }
 
 function niceSize(){
-	alert('resize');
+	//alert('resize');
 	if(document.documentElement.clientWidth * 0.01 * parseFloat(window.getComputedStyle(document.getElementById('niceSize')).getPropertyValue('--vwMultiplier')) > parseFloat(getComputedStyle(document.documentElement).fontSize) * parseFloat(window.getComputedStyle(document.getElementById('niceSize')).getPropertyValue('--remMultiplier'))){
 		document.querySelector('body').style.setProperty('--nSMaxWidth', parseFloat(getComputedStyle(document.documentElement).fontSize) * parseFloat(window.getComputedStyle(document.getElementById('niceSize')).getPropertyValue('--remMultiplier')) + 'px');
 	}else{
@@ -56,13 +56,20 @@ function niceSize(){
 	document.getElementById('niceSize').style.setProperty('display', 'flex');
 	if(mobile==1 && window.innerWidth != widthOld){
 		document.querySelector('body').style.setProperty('--nSHeight', window.innerHeight * 0.01 + 'px');
-		alert('resize if');
-		alert(widthOld);
+		//alert('resize if');
+		//alert(widthOld);
 		widthOld = window.innerWidth;
-		alert(widthOld);
+		//alert(widthOld);
 	}
 	if(mobile==0){
 		document.querySelector('body').style.setProperty('--nSHeight', '1vh');
 	}
 	document.getElementById('niceSize').style.setProperty('display', 'none');
+}
+
+
+window.addEventListener('scroll', () => {window.addEventListener('resize', niceSizetest);});
+
+function niceSizetest(){
+	alert('');
 }
